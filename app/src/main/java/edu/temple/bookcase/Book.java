c7b099047668de0ed48be5b2030a13990557aa07
Luke Jakielaszek <tug52339@temple.edu>
2019-11-03 19:25:11 -0500

    Created book class which implements Pacelable

---
+++ app/src/main/java/edu/temple/bookcase/Book.java
@@ -0,0 +1,54 @@
+package edu.temple.bookcase;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
+    private int id;
+    private String title;
+    private String author;
+    private int published;
+    private String coverURL;
+
+    public Book(int id, String title, String author, int published, String coverURL){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.coverURL = coverURL;
+    }
+
+    protected Book(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        author = in.readString();
+        published = in.readInt();
+        coverURL = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
+        parcel.writeString(author);
+        parcel.writeInt(published);
+        parcel.writeString(coverURL);
+    }
+}
