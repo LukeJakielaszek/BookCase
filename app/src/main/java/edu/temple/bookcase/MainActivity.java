32a4f115dd2460be42d9ff853937af545f988cbf
Luke Jakielaszek <tug52339@temple.edu>
2019-11-05 13:51:48 -0500

    Got JSON object from web for book list

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -1,12 +1,24 @@
 package edu.temple.bookcase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,10 +28,60 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
     ArrayList<String> bookList;
     boolean singlePane;
 
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+            try{
+                Log.d("MyApplication", message.obj.toString());
+                JSONArray webPage = new JSONArray(message.obj.toString());
+
+                JSONObject book_web = new JSONObject(webPage.getJSONObject(2).toString());
+                Log.d("MyApplication", book_web.getString("author"));
+
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            return false;
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                URL url = null;
+
+                try{
+                    url = new URL("https://kamorris.com/lab/audlib/booksearch.php");
+                    BufferedReader reader = null;
+
+                    reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                    StringBuilder builder = new StringBuilder();
+                    String response;
+
+                    response = reader.readLine();
+                    while(response != null){
+                        builder.append(response);
+                        response = reader.readLine();
+                    }
+
+                    Log.d("MyApplication", builder.toString());
+                    Message message = Message.obtain();
+                    message.obj = builder.toString();
+
+                    handler.sendMessage(message);
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
         Log.d("MyApplication", "Grabbing book list");
 
@@ -31,7 +93,6 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
 
 
         Log.d("MyApplication", "Initialized BookList");
-
 
         if(singlePane){
             Log.d("MyApplication", "single_pane");
