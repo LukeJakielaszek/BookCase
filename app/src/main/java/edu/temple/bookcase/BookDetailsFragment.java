ab00a24438cb037d9b759f8389fcb0ee86e62b34
Luke Jakielaszek <tug52339@temple.edu>
2019-11-07 11:48:33 -0500

    Successfully set image using thread and handler

--- app/src/main/java/edu/temple/bookcase/BookDetailsFragment.java
+++ app/src/main/java/edu/temple/bookcase/BookDetailsFragment.java
@@ -1,16 +1,26 @@
 package edu.temple.bookcase;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +30,8 @@ public class BookDetailsFragment extends Fragment {
     TextView titleTextView;
     TextView authorTextView;
     TextView publishedTextView;
+    ImageView bookImageView;
+    String url;
 
     public static final String BOOK_KEY = "book";
 
@@ -57,11 +69,22 @@ public class BookDetailsFragment extends Fragment {
         titleTextView = v.findViewById(R.id.bookTitle);
         authorTextView = v.findViewById(R.id.bookAuthor);
         publishedTextView = v.findViewById(R.id.bookPublish);
+        bookImageView = v.findViewById(R.id.bookImage);
 
         displayBook(this.book);
 
         return v;
     }
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+            Bitmap bm = (Bitmap)message.obj;
+            BookDetailsFragment.this.bookImageView.setImageBitmap(bm);
+
+            return false;
+        }
+    });
 
     public void displayBook(Book book){
         titleTextView.setText(book.getTitle());
@@ -70,5 +93,25 @@ public class BookDetailsFragment extends Fragment {
         authorTextView.setText(book.getAuthor());
 
         publishedTextView.setText(Integer.toString(book.getPublished()));
+        url = book.getCoverURL();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try
+                {
+                    InputStream imageURL = (InputStream) new URL(BookDetailsFragment.this.url).getContent();
+                    Bitmap bitmap = BitmapFactory.decodeStream(imageURL);
+
+                    Message message = Message.obtain();
+                    message.obj = bitmap;
+                    handler.sendMessage(message);
+                }catch(
+                        IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
